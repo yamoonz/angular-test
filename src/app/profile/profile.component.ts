a2c52ef7838f682e2c84212b245df86b6b7b1cee
Yamoonz <yamooonz@gmail.com>
2021-01-11 15:00:47 +0300

    --- Auto-Git Commit ---

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { OnDestroy } from '@angular/core';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit ,OnDestroy {
   userId: number= null;
-private sub: Subscription;
-user: User=null;
+  private sub: Subscription;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.sub= this.activatedRoute.params.subscribe(params=>{
+      this.userId = parseInt(params['id']);
+
+    });
   }
 
+    ngOnDestroy (){
+      this.sub.unsubscribe();
+
+    }
 }
