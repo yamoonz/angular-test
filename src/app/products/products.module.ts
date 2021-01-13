9e80756be71261cc8fcc8dd865e34cdf17fc0524
Yamoonz <yamooonz@gmail.com>
2021-01-13 12:53:55 +0300

    --- Auto-Git Commit ---

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,9 +2,11 @@ export class Products{
     public name: string;
     public description: string;
     public imagePath: string;
+    public lastUpdated: string;
 
-    constructor (name: string, desc:string, imagePath: string){
+    constructor (name: string, desc:string, imagePath: string, lastUpdated:string){
         this.name= name;
+        this.lastUpdated= lastUpdated;
         this.description= desc;
         this.imagePath=imagePath;
     }
