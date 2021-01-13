802a29b8e13ea0317b29fdaa85f4a55a395dc63b
Yamoonz <yamooonz@gmail.com>
2021-01-13 15:54:36 +0300

    --- Auto-Git Commit ---

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,17 @@ export class AppComponent {
       sales :"371 sales",
       lastUpdated:" last updated: 16 Dec 20",
       imagePath:"https://tutsflow.com/wp-content/uploads/2020/12/cartzilla-wp-theme.jpg",
-    }
+    },
+    {
+      name : "Cartzilla - Digital Market",
+      title : "by MadrasThemes in Wooco ",
+      description:"Multi Vendor Digital Market Place With Dokan",
+      price:"59$",
+      rating:"five stars (22) ",
+      sales :"371 sales",
+      lastUpdated:" last updated: 16 Dec 20",
+      imagePath:"https://tutsflow.com/wp-content/uploads/2020/12/cartzilla-wp-theme.jpg",
+    },
   ]
   onNavigate(feature: string) {
     this.loadedFeature = feature;
