c86334ab4abd9ff13a657392ece0817b2e238a56
Yamoonz <yamooonz@gmail.com>
2021-01-13 16:24:44 +0300

    --- Auto-Git Commit ---

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators'; 
-import { title } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +13,7 @@ export class AppComponent {
     {
       name : "Cartzilla - Digital Market",
       title : "by MadrasThemes in Wooco ",
+      tags:"Electronics, Foo, Bar ",
       description:"Multi Vendor Digital Market Place With Dokan",
       price:"59$",
       rating:"five stars (22) ",
@@ -22,25 +22,38 @@ export class AppComponent {
       imagePath:"https://tutsflow.com/wp-content/uploads/2020/12/cartzilla-wp-theme.jpg",
     },
     {
-      name : "Cartzilla - Digital Market",
-      title : "by MadrasThemes in Wooco ",
-      description:"Multi Vendor Digital Market Place With Dokan",
-      price:"59$",
+      name : "Special Offer",
+      title : "Home For Sell ",
+      tags:"Home, Foo, Bar ",
+      description:"Lorem Spum",
+      price:"58999$",
       rating:"five stars (22) ",
-      sales :"371 sales",
-      lastUpdated:" last updated: 16 Dec 20",
-      imagePath:"https://tutsflow.com/wp-content/uploads/2020/12/cartzilla-wp-theme.jpg",
+      sales :"71 sales",
+      lastUpdated:" last updated: 13 Jan 21",
+      imagePath:"https://www.propertyturkey.com/uploads/realestate/larg/bursa_villa_2_5.jpg",
+    },
+    {
+      name : "Special Offer",
+      title : "Kitchen For Sell ",
+      tags:"Kitchen, Foo, Bar ",
+      description:"Lorem Spum",
+      price:"999$",
+      rating:"five stars (22) ",
+      sales :"50 sales",
+      lastUpdated:" last updated: 13 Jan 21",
+      imagePath:"https://i.pinimg.com/originals/f4/1a/17/f41a179d2fc15c79a65e81f3d3843a96.jpg",
     },
     {
       name : "Cartzilla - Digital Market",
       title : "by MadrasThemes in Wooco ",
+      tags:"Electronics, Foo, Bar ",
       description:"Multi Vendor Digital Market Place With Dokan",
       price:"59$",
       rating:"five stars (22) ",
       sales :"371 sales",
       lastUpdated:" last updated: 16 Dec 20",
       imagePath:"https://tutsflow.com/wp-content/uploads/2020/12/cartzilla-wp-theme.jpg",
-    },
+    }
   ]
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,11 +3,13 @@ export class Products{
     public description: string;
     public imagePath: string;
     public lastUpdated: string;
+    public tags:string;
 
-    constructor (name: string, desc:string, imagePath: string, lastUpdated:string){
+    constructor (name: string, desc:string, imagePath: string, lastUpdated:string, tags:string){
         this.name= name;
         this.lastUpdated= lastUpdated;
         this.description= desc;
         this.imagePath=imagePath;
+        this.tags=tags;
     }
 }
